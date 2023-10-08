6fb603688b1c67d859b115f861975b9e4a262410
michelmorin <michelmorin77@gmail.com>
2023-10-07 20:33:51 -0400

    remove deployed vpc

--- vpc.tf
+++ vpc.tf
@@ -1,17 +1,17 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+#module "vpc" {
+#  source = "terraform-aws-modules/vpc/aws"
 
-  name = "vpc-${var.PROJECT_NAME}"
-  cidr = "10.0.0.0/16"
+#  name = "vpc-${var.PROJECT_NAME}"
+#  cidr = "10.0.0.0/16"
 
-  azs            = ["${var.AWS_REGION}a", "${var.AWS_REGION}b", "${var.AWS_REGION}c"]
-  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+#  azs            = ["${var.AWS_REGION}a", "${var.AWS_REGION}b", "${var.AWS_REGION}c"]
+#  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = false
-  enable_vpn_gateway = false
+#  enable_nat_gateway = false
+#  enable_vpn_gateway = false
 
-  tags = {
-    Terraform = "true"
-    project   = "${var.PROJECT_NAME}"
-  }
-}
+#  tags = {
+#    Terraform = "true"
+#    project   = "${var.PROJECT_NAME}"
+#  }
+#}
